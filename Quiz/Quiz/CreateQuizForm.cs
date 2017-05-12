c97c309ed75b0d8ae75b3b1ef49cfbced909507c
ktfjulien <ktfjulien@gmail.com>
2017-05-12 14:57:58 -0700

    Created the actual form for quiz creation

---
+++ Quiz/Quiz/CreateQuizForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz {
+    public partial class CreateQuizForm : Form {
+        public CreateQuizForm() {
+            InitializeComponent();
+        }
+    }
+}

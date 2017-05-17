757cd1f26e1379597c6ddb21bb453b0a900be5ba
Julien <ktfjulien@gmail.com>
2017-05-17 12:32:36 -0700

    form stuff

--- Quiz/Quiz/CreateQuizForm.Designer.cs
+++ Quiz/Quiz/CreateQuizForm.Designer.cs
@@ -23,13 +23,31 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.QuizIDTB = new System.Windows.Forms.TextBox();
+            this.QuizNameTB = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
+            // 
+            // QuizIDTB
+            // 
+            this.QuizIDTB.Location = new System.Drawing.Point(222, 93);
+            this.QuizIDTB.Name = "QuizIDTB";
+            this.QuizIDTB.Size = new System.Drawing.Size(100, 20);
+            this.QuizIDTB.TabIndex = 0;
+            // 
+            // QuizNameTB
+            // 
+            this.QuizNameTB.Location = new System.Drawing.Point(222, 149);
+            this.QuizNameTB.Name = "QuizNameTB";
+            this.QuizNameTB.Size = new System.Drawing.Size(100, 20);
+            this.QuizNameTB.TabIndex = 1;
             // 
             // CreateQuizForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(580, 452);
+            this.Controls.Add(this.QuizNameTB);
+            this.Controls.Add(this.QuizIDTB);
             this.HelpButton = true;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -37,9 +55,13 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Create Quiz";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox QuizIDTB;
+        private System.Windows.Forms.MaskedTextBox QuizNameTB;
     }
 }

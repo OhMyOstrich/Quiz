0cb36a33c2be32cc63c231c10cb56eb73691af8b
Andrew Jenkins <jenkinsandrew2319@gmail.com>
2017-06-07 04:33:27 -0700

    Score Report stuff

--- Quiz/Quiz/Forms/ScoreForm.Designer.cs
+++ Quiz/Quiz/Forms/ScoreForm.Designer.cs
@@ -28,24 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScoreForm));
+            this.SaveReport = new System.Windows.Forms.Button();
             this.ExitBtn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.ScoreLabel = new System.Windows.Forms.Label();
+            this.QuizTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // SaveReport
             // 
-            this.button1.Location = new System.Drawing.Point(108, 181);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.SaveReport.Location = new System.Drawing.Point(166, 210);
+            this.SaveReport.Name = "SaveReport";
+            this.SaveReport.Size = new System.Drawing.Size(75, 23);
+            this.SaveReport.TabIndex = 0;
+            this.SaveReport.Text = "Save ";
+            this.SaveReport.UseVisualStyleBackColor = true;
+            this.SaveReport.Click += new System.EventHandler(this.SaveReport_Click);
             // 
             // ExitBtn
             // 
-            this.ExitBtn.Location = new System.Drawing.Point(108, 210);
+            this.ExitBtn.Location = new System.Drawing.Point(50, 210);
             this.ExitBtn.Name = "ExitBtn";
             this.ExitBtn.Size = new System.Drawing.Size(75, 23);
             this.ExitBtn.TabIndex = 1;
@@ -56,32 +59,49 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(105, 21);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.Size = new System.Drawing.Size(116, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Score Report";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // ScoreLabel
             // 
             this.ScoreLabel.AutoSize = true;
-            this.ScoreLabel.Location = new System.Drawing.Point(125, 91);
+            this.ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.Location = new System.Drawing.Point(119, 124);
             this.ScoreLabel.Name = "ScoreLabel";
-            this.ScoreLabel.Size = new System.Drawing.Size(35, 13);
+            this.ScoreLabel.Size = new System.Drawing.Size(51, 20);
             this.ScoreLabel.TabIndex = 3;
             this.ScoreLabel.Text = "label2";
+            this.ScoreLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // QuizTitle
+            // 
+            this.QuizTitle.AutoSize = true;
+            this.QuizTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuizTitle.Location = new System.Drawing.Point(102, 61);
+            this.QuizTitle.Name = "QuizTitle";
+            this.QuizTitle.Size = new System.Drawing.Size(84, 20);
+            this.QuizTitle.TabIndex = 4;
+            this.QuizTitle.Text = "Quiz Title";
+            this.QuizTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // ScoreForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.QuizTitle);
             this.Controls.Add(this.ScoreLabel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ExitBtn);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SaveReport);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ScoreForm";
-            this.Text = "ScoreForm";
+            this.Text = "Score";
             this.Load += new System.EventHandler(this.ScoreForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -90,9 +110,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button SaveReport;
         private System.Windows.Forms.Button ExitBtn;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Label QuizTitle;
     }
 }

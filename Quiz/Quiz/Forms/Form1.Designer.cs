15137f86ebed06f750e754b9228b3254e1e40d53
Jacob Loren Eli Tart <mrjtart@yahoo.com>
2017-06-07 11:00:00 -0700

    Changed open form and fixed another thing that i cant rememeber right now

--- Quiz/Quiz/Forms/Form1.Designer.cs
+++ Quiz/Quiz/Forms/Form1.Designer.cs
@@ -31,7 +31,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.TempQuestionButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -40,11 +41,12 @@
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.button1.Location = new System.Drawing.Point(0, 367);
+            this.button1.Location = new System.Drawing.Point(0, 286);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(426, 35);
+            this.button1.Size = new System.Drawing.Size(385, 35);
             this.button1.TabIndex = 0;
             this.button1.Text = "Create Quiz";
             this.button1.UseVisualStyleBackColor = false;
@@ -60,21 +62,21 @@
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
             this.button2.Location = new System.Drawing.Point(0, 0);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(426, 78);
+            this.button2.Size = new System.Drawing.Size(385, 83);
             this.button2.TabIndex = 1;
             this.button2.Text = "Take Quiz";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // TempQuestionButton
+            // pictureBox1
             // 
-            this.TempQuestionButton.Location = new System.Drawing.Point(0, 288);
-            this.TempQuestionButton.Name = "TempQuestionButton";
-            this.TempQuestionButton.Size = new System.Drawing.Size(426, 71);
-            this.TempQuestionButton.TabIndex = 2;
-            this.TempQuestionButton.Text = "Question?";
-            this.TempQuestionButton.UseVisualStyleBackColor = true;
-            this.TempQuestionButton.Click += new System.EventHandler(this.TempQuestionButton_Click);
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(19)))), ((int)(((byte)(127)))));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 83);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(388, 204);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
@@ -82,8 +84,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Azure;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(426, 402);
-            this.Controls.Add(this.TempQuestionButton);
+            this.ClientSize = new System.Drawing.Size(385, 321);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Cursor = System.Windows.Forms.Cursors.Default;
@@ -100,6 +102,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cheney Public Schools Testing";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -108,7 +111,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button TempQuestionButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
